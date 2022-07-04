075c2e6b54ee69c5d23c5b845e360ba7c8486c45
iandrc <iandrc9@gmail.com>
2022-07-04 02:38:38 +0200

    chore: Run `rustfmt` on source files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
+
     println!("Please, input your guess.");
 
     let mut guess = String::new();
